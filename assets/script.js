8db111ff025e819359b7de0949c2a66902f74a1d
Austin Oler <oleraustin@gmail.com>
2023-10-03 19:28:49 -0400

    added questions for quiz on js

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+// Define an array of questions with choices and correct answers
+var questions = [
+    {
+      question: "What does HTML stand for",
+      choices: ["Hyper Transfer Markup Language", "Hyper Text Makeup Language","Hyperlink and Text Markup Language", "Hyper Text Markup Language"],
+      correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+      question: "Which programming language is used for web development?",
+      choices: ["Python", "Java", "HTML", "C++"],
+      correctAnswer: "HTML",
+    },
+    {
+        question: "Which HTML tag is used for creating an unordered list?",
+        choices: ["<ol>", "<ul>", "<dl>", "<li>"],
+        correctAnswer: "<ul>",
+    },
+    {
+        question: "What property in CSS is used to change the text color of an element?",
+        choices: ["Text-color", "font-color", "color", "text-style"],
+        correctAnswer: "color",
+    },
+    {
+        question: "Which CSS selector targets elements with a specific class",
+        choices: ["*myClass", "element.class", "#myClass","myClass"],
+        correctAnswer: "myClass",
+    },
+    {
+        question: "What keyword is used to declare a variable in JavaScript?",
+        choices: ["var", "variable", "let", "v"],
+        correctAnswer: "var",
+    },
+    {
+        question: "Which operator is used to compare two values for equality without checking data type in JavaScript?",
+        choices: ["===", "==", "!=", "!=="],
+        correctAnswer: "==",
+    },
+    {
+        question: "How do you add an event listener to an HTML element using JavaScript?",
+        choices: ["element.addEventListener('event', function)", "element.onEvent('event', function)", "element.attachEvent('event', function)", "element.addEvent('event', function)"],
+        correctAnswer: "element.addEventListener('event', function)"
+    }
+  ];
